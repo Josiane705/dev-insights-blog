861474e0d563ac68808a76ad2524c59d72ce5e91
Josiane705 <j.mukeshima@alustudent.com>
2026-09-21 21:53:50 +0200

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Dev Insights</h1>
+      <nav>
+        <a href="#">New Post</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
